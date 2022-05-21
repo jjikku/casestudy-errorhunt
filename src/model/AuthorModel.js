456e84b736df06cd7b3d3ce40b2ee99a7aec2520
jjikku <98324520+jjikku@users.noreply.github.com>
2022-05-21 15:53:53 +0530

    Removed unified topology

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/Library?directConnection=true", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+  //useUnifiedTopology: true,
+  useNewUrlParser: true
 });
 const Schema = mongoose.Schema;
 
--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/Library?directConnection=true", {
-  useUnifiedTopology: true,
+  //useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 const Schema = mongoose.Schema;
